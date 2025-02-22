03e1eb580d9c38129501b69d597ddbdff7d58584
Yodabytz <122388115+yodabytz@users.noreply.github.com>
2025-02-22 11:39:18 -0500

    Create irc.py

---
+++ irc.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import socket
+import ssl
+from config import server, port, channels, botnick, use_ssl
+
+def connect_and_register():
+    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if use_ssl:
+        context = ssl.create_default_context()
+        irc = context.wrap_socket(irc, server_hostname=server)
+    irc.connect((server, port))
+    irc.send(f"NICK {botnick}\r\n".encode("utf-8"))
+    irc.send(f"USER {botnick} 0 * :Python IRC Bot\r\n".encode("utf-8"))
+    connected = False
+    while not connected:
+        response = irc.recv(2048).decode("utf-8", errors="ignore")
+        print(response)
+        for line in response.split("\r\n"):
+            if line.startswith("PING"):
+                irc.send(f"PONG {line.split()[1]}\r\n".encode("utf-8"))
+            if " 001 " in line:  # Successful registration
+                connected = True
+                for chan in channels:
+                    irc.send(f"JOIN {chan}\r\n".encode("utf-8"))
+                    send_message(irc, chan, "FuzzyFeeds has joined the channel!")
+    return irc
+
+def send_message(irc, target, message):
+    irc.send(f"PRIVMSG {target} :{message}\r\n".encode("utf-8"))
+
+def send_private_message(irc, user, message):
+    # Send a private message directly to the user.
+    irc.send(f"PRIVMSG {user} :{message}\r\n".encode("utf-8"))
+
+def send_multiline_message(irc, user, message):
+    # Splits the message by newline and sends each line as a private message.
+    for line in message.splitlines():
+        send_private_message(irc, user, line)
